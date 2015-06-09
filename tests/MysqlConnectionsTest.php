0c44b84bf88aea501849274faecd516bdb921213
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-06-09 22:32:30 +0200

    tests refactory

--- tests/MysqlConnectionsTest.php
+++ tests/MysqlConnectionsTest.php
@@ -19,9 +19,6 @@ use it\thecsea\mysqltcs\connections\MysqlConnections;
  * @package it\thecsea\mysqltcs
  */
 class MysqlConnectionsTest extends \PHPUnit_Framework_TestCase {
-    /**
-     * @expectedException \it\thecsea\mysqltcs\connections\MysqlConnectionException
-     */
     public function testOneRemove()
     {
         $db = include(__DIR__."/config.php");
@@ -29,21 +26,7 @@ class MysqlConnectionsTest extends \PHPUnit_Framework_TestCase {
         $obj = new Mysqltcs($db['host'], $db['user'], $db['psw'], $db['db']);
         $mysqlConnections->getConnection($obj, $db['host'], $db['user'], $db['psw'], $db['db']);
         $mysqlConnections->removeClient($obj);
-        $this->assertTrue(true); //test no exception in previous instruction
+        $this->setExpectedException('\it\thecsea\mysqltcs\connections\MysqlConnectionException');
         $mysqlConnections->removeClient($obj);
     }
-
-    /*
-     *  $this->assertTrue(true);
-
-        $db = include(__DIR__."/config.php");
-        $mysqlConnections = MysqlConnections::getInstance();
-        $obj1 = new \stdClass();
-        $conn1 = $this->mysqlConnections->getConnection($obj1, $this->host, $this->user, $this->password, $this->name, $this->key, $this->cert, $this->ca);
-        $obj2 = new \stdClass();
-        $conn2 = $this->mysqlConnections->getConnection($obj2, $this->host, $this->user, $this->password, $this->name, $this->key, $this->cert, $this->ca);
-        $connection = new MysqlConnection($db['host'],  $db['user'], $db['psw'], $db['db']);
-        $expected = ("host: ".$db['host'] ."\nuser: ".$db['user'] ."\npassword: ".$db['psw']  ."\nname: ".$db['db'] ."\nkey: "."" ."\ncert: "."" ."\nca: "."");
-        $this->assertEquals((String)$connection , $expected);
-     */
 }
